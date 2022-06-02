604b3f1067da858ed8329e277c254808b2250309
DESKTOP-J743OV8\local user <trinhtuan2801@gmail.com>
2022-06-02 18:43:31 +0700

    mui v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import { makeStyles } from "@material-ui/core"
+import { Box } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ReviewPage from "./Pages/ReviewPage";
 import SearchResultPage from "./Pages/SearchResultPage";
 
-const useStyles = makeStyles((theme) => ({
-  app: {
-    width: '100%',
-    height: 'fit-content',
-    padding: 0,
-    margin: 0,
-    position: 'relative',
-    backgroundColor: '#fff',
-  },
-}))
 
 const App = () => {
-  const classes = useStyles()
 
   return (
-    <div className={classes.app}>
+    <Box
+      sx={{
+        width: '100%',
+        height: 'fit-content',
+        padding: 0,
+        margin: 0,
+        position: 'relative',
+        backgroundColor: '#fff',
+      }}
+    >
       <Routes>
         <Route path="*" element={<Navigate to='/' replace />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchResultPage />} />
         <Route path="/review" element={<ReviewPage />} />
       </Routes>
-    </div>
+    </Box>
   )
 }
 
